e54d1c9c1417e7ba07ae99e49d87630ef642b39f
Manny Martinez <vccengineers12@gmail.com>
2018-10-30 03:44:04 -0700

    added donation button

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Button } from "semantic-ui-react";
 
 import '../styles/Footer.css';
 
 const date = new Date();
 let year = date.getFullYear();
 
-class Footer extends Component {
-	render() {
-		return (
-			<div className="footer-container">
-				<div className="copyright-container">
-					<p>Copyright © {year} Garbage Time Pro</p>
-					<div className="social-icons">
-						<a href="http://twitter.com/garbagetimepro"><i className="fa fa-twitter" /></a>
-						<a href="http://facebook.com/garbagetimepro"><i className="fa fa-facebook" /></a>
-						<a href="http://instagram.com/garbagetimepro"><i className="fa fa-instagram" /></a>
-					</div>
+const Footer = () => {
+	return (
+		<div className="footer-container">
+			<div className="copyright-container">
+				<p>Copyright © {year} Garbage Time Pro</p>
+				
+				<div className="social-icons">
+					<a className="donate-link" href="https://www.paypal.me/mmdevs">
+						<i className="fa fa-paypal" />
+						&nbsp;Donate
+					</a>
+					{/* <span style={{fontSize: '16px'}}>|</span>
+					<a href="http://twitter.com/garbagetimepro"><i className="fa fa-twitter" /></a>
+					<a href="http://facebook.com/garbagetimepro"><i className="fa fa-facebook" /></a>
+					<a href="http://instagram.com/garbagetimepro"><i className="fa fa-instagram" /></a> */}
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 };
 
 export default Footer;
